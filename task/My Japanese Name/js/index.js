6c02d1c540b70f5c4f51948a264f01f4ba73f782
R.Otsubo <otsubo.seraku@gmail.com>
2018-05-09 17:22:31 +0900

    add slider(slick)

--- task/My Japanese Name/js/index.js
+++ task/My Japanese Name/js/index.js
@@ -5,6 +5,39 @@ $('.slider_main').slick({
   dotsClass:'slick-dots'
 });
 $(".slider_main button").remove();
-
+var slider = $('.slider_steps');
+var navi = $('.slider_navi ul');
+var navi_li = $('.slider_navi ul li');
 //steps-slick
-
+  //index番号を付与
+  $('.slider_navi ul li').each(function(){
+     slideIndex = $(this).index();
+     $(this).attr('slideIndex',slideIndex);
+    });
+//スライダー本体
+  $('.slider_steps').slick({
+    autoplay:true
+  });
+//現在のスライドのindexを取得
+var length = $('.slider_navi ul li').length;
+var current_slide ;
+//スライド移動の直前に発火
+$(slider).on('beforeChange',function(){
+  var slideIndex = slider.slick('slickCurrentSlide') + 1;
+  var current_slide ;
+  if(slideIndex < length){
+    current_slide = slideIndex ;
+  }else{
+    current_slide = 0 ;
+  }
+  for(i=0 ; i<=length ; i++){
+    var navi_index = $(navi_li).eq(i).attr('slideIndex');
+    var navieq = $(navi_li).eq(i);
+    //スライドのindexとサムネイルのindexが同じ場合にクラスactiveを追加する。
+  if(navi_index == current_slide){
+      $(navieq).addClass('active')
+  }else{
+    $(navieq).removeClass('active')
+  };
+    };
+});
